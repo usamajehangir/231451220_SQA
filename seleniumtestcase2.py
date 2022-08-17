eaa00b676b23ed44bc1a0205aeb9df73b21c7369
usamajehangir <102380110+usamajehangir@users.noreply.github.com>
2022-08-18 00:54:40 +0500

    Delete seleniumtestcase2.py

--- seleniumtestcase2.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome(executable_path="C:\Windows\chromedriver.exe")
-driver.get("https://www.facebook.com")
-print(driver.title)
-driver.close()
